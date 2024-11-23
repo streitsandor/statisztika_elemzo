79ea3a2cd78e5ce34caf54c29cd6c06ac218f0bf
Streit Sándor <sandorstreit@gmail.com>
2024-11-24 00:09:40 +0100

    v0.2.0

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 from icecream import ic
 
-os.system("cls" if os.name == "nt" else "clear")  # konzol tisztítása
-
 # print(plt.style.available)  # stílus keresésre, ha kell
 
 
+def clear_console() -> None:  # konzol tisztítása
+    os.system("cls" if os.name == "nt" else "clear")
+
+
 def main() -> None:
+    clear_console()
     diagramok = {1: diagram1, 2: diagram2}  # Választható diagramok
     plt.tight_layout()
 
@@ -20,9 +23,10 @@ def main() -> None:
                 input(
                     """
                     ==============================================
-                    0. Kilépés
-                    1. Átlagkereset foglalkozás szerint - Teljes munkaidő (bruttó)
-                    2. Placeholder
+                        0. Kilépés
+                        
+                        1. Átlagkereset foglalkozás szerint - Teljes munkaidő (bruttó)
+                        2. Placeholder
                     ==============================================
                     
                     Válasszon egy diagramot: """
@@ -32,15 +36,25 @@ def main() -> None:
                 break
 
             if statisztika_szama in diagramok:  # Kiválasztott diagram futtatása
+                clear_console()
                 diagramok[statisztika_szama]()
             else:
-                print("Nem megfelelő számot adott meg! (1-4)")
+                clear_console()
+                print("Nem megfelelő számot adott meg!")
         except ValueError:
-            print("Érvénytelen karakter. Kérem számot adjon meg! (1-4)")
+            clear_console()
+            print("Érvénytelen karakter. Kérem számot adjon meg!")
 
 
-def comma_formatter(x, pos) -> str:  # integer formázó
+def integer_formazo(x, pos) -> str:  # integer formázó
     return "{:0,d}".format(int(x))
+
+
+def lista_darabolas(bemeneti_lista, darab_meret) -> list:  # lista daraboló
+    visszatero_lista = []
+    for i in range(0, len(bemeneti_lista), darab_meret):
+        visszatero_lista.append(bemeneti_lista[i : i + darab_meret])
+    return visszatero_lista
 
 
 def diagram1() -> None:
@@ -51,49 +65,51 @@ def diagram1() -> None:
     csv_adatok = pd.read_csv(
         "import/stadat-mun0208-20.1.1.52-hu_tisztitott.csv", encoding="UTF-8"
     )
-    fejlec_adatok = list(csv_adatok)[2:]
+    fejlec_adatok = list(csv_adatok)[2:]  # fejléc összeállítása
+    darabolt_listak = lista_darabolas(csv_adatok, 10)  # adatok feldarabolása
 
     while True:
         try:
+            print(
+                f"""
+                    **********************************************
+                        0. Vissza a főmenübe
+                        
+                        Összesen {len(darabolt_listak)} lista áll rendelkezésre.
+                    **********************************************
+                """
+            )
             alszam = int(
                 input(
                     """
-                    
-                    **********************************************
-                    0. Vissza a főmenübe
-                    1. Lista 0-10
-                    2. Lista 10-20
-                    **********************************************
-                    
-                    Válasszon egy alszámot: """
+                    Válassza ki a megjelenítendő listát: """
                 )
             )
-            match alszam:
-                case 0:
-                    break
-                case 1:
-                    uj_param = {
-                        **alap_param,
-                        "adat": csv_adatok.iloc[:11],
-                        "fejlec_adatok": fejlec_adatok,
-                        "title": "0-10",
-                    }
-                    diagram1_kirajzolo(uj_param)
-                case 2:
-                    uj_param = {
-                        **alap_param,
-                        "adat": csv_adatok.iloc[10:21].reset_index(drop=True),
-                        "fejlec_adatok": fejlec_adatok,
-                        "title": "10-20",
-                    }
-                    diagram1_kirajzolo(uj_param)
-                case _:
-                    print("Nem megfelelő alszámot adott meg!")
+
+            if alszam == 0:
+                clear_console()
+                break
+            elif alszam > 0 and alszam <= len(
+                darabolt_listak
+            ):  # kiválasztott tábla paraméterezése
+                clear_console()
+                uj_param = {
+                    **alap_param,
+                    "adat": darabolt_listak[alszam - 1].reset_index(drop=True),
+                    "fejlec_adatok": fejlec_adatok,
+                    "title": str(alszam) + ". táblázat",
+                }
+                diagram1_kirajzolo(uj_param)
+            else:
+                clear_console()
+                print("Nem megfelelő számot adott meg!")
+
         except ValueError:
+            clear_console()
             print("Érvénytelen karakter. Kérem számot adjon meg!")
 
 
-def diagram1_kirajzolo(parameter) -> None:
+def diagram1_kirajzolo(parameter) -> None:  # kiválasztott tábla rajzolása
     for i in range(len(parameter["adat"])):
         plt.plot(
             parameter["fejlec_adatok"],
@@ -110,7 +126,7 @@ def diagram1_kirajzolo(parameter) -> None:
 
     plt.title(parameter["title"])
     plt.ticklabel_format(style="plain", axis="y")
-    plt.gca().yaxis.set_major_formatter(FuncFormatter(comma_formatter))
+    plt.gca().yaxis.set_major_formatter(FuncFormatter(integer_formazo))
     plt.grid(color="green", linestyle="--", linewidth=0.5)
     plt.legend()
 
